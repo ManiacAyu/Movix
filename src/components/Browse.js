b5f21c1c4569051e117afc6722d8401b5b279d4a
ManiacAyu <Ayush Dhoke>
2024-06-21 00:28:21 +0530

    commit 10

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Header from './Header'
 
 const Browse = () => {
   return (
-    <div>
-      
+    <div >
+      <Header/>
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { removeUser } from "../utils/userSlice";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const signOutHandler = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");      
+      })
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
-      <img className = "w-44" src="/logo.png" alt="logo"></img>
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+      <img className="w-44" src="/logo.png" alt="logo"></img>
+
+      <div className="flex p-4">
+        <img className="w-12 h-12" alt="usericon" src="/userIcon.jpg" />
+        <button
+          onClick={signOutHandler}
+          className="font-bold text-red-600 ml-2"
+        >
+          Sign Out
+        </button>
+      </div>
     </div>
   );
 };
